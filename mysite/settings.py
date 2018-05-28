1f4245fa8d2f470e84c1d9d1176bb44afa56557e
AravindSarathChandraKumar <aravinds95@gmail.com>
2018-05-28 20:56:12 +0530

    detailview correction

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'personal',
+    'blog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,13 +75,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+DATABASES ={
+    'default':{
+        'ENGINE':'django.db.backends.mysql',
+        'NAME':'personal',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'localhost',
+        'PORT':''
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
--- personal/views.py
+++ personal/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'personal/home.html')
+
+def contact(request):
+    return render(request,'personal/basic.html',{'content':['Email ME :','user@gmail.com']})
